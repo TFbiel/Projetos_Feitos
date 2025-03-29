2675649cfc0b402ca7631401f168c015d886f329
Gabriel Ferreira <Afgabriel20@gmail.com>
2025-03-29 00:09:59 -0300

    Update index.js

--- Java com Node/src/index.js
+++ Java com Node/src/index.js
@@ -9,14 +9,6 @@ fs.readFile(link, 'utf-8', (erro, texto) => {
    verificaPalavrasDuplicadas(texto);
 })
 
-//Criar um array com as palavras
-// Contar as ocorrências
-// Montar um objeto com o resultado
-
-//{
-  // "web": 5,
-  //  "computador": 4}
-
 function verificaPalavrasDuplicadas (texto){
    const listaPalavras = texto.split(' ');
    const resultado = {};
